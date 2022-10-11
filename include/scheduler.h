4bccaf97cd0032639a91ea6b3c4cc6bbd61b3d21
JanTomassi <tomassijan@gmail.com>
2022-10-11 14:14:52 +0200

    Fix commit error

--- include/scheduler.h
+++ include/scheduler.h
@@ -1,18 +1,8 @@
 #ifndef __SCHEDULER_H
 #define __SCHEDULER_H
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-#include <stddef.h>
 #include <stdint.h>
-=======
-#include "stm32f4_discovery.h"
-#include "sys.h"
->>>>>>> 91c65aa (First implementation of a non preemptive Scheduler)
-=======
-#include <stddef.h>
-#include <stdint.h>
->>>>>>> 2e4e481 (Remove unused header from scheduler)
+
 /**
  * @brief Number of max scheduled task
  * @warning Max is 127
@@ -21,8 +11,6 @@
 
 typedef struct task
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
     uint32_t period;            // Rate at which the task should tick
     uint32_t elapsedTime;       // Time since task's last tick
     void (*TickFct)(void); // Function to call for task's tick
@@ -31,26 +19,6 @@ typedef struct task
 const int16_t schAddTask(const uint32_t period, void (*TickFct)());
 const int16_t schAddIndexTask(const uint32_t period, uint8_t task_num, void (*TickFct)());
 void schRemoveTask(uint8_t task_num);
-=======
-    u32 period;            // Rate at which the task should tick
-    u32 elapsedTime;       // Time since task's last tick
-    void (*TickFct)(void); // Function to call for task's tick
-} task;
-
-sc16 schAddTask(uc32 period, void (*TickFct)());
-sc16 schAddIndexTask(uc32 period, u8 task_num, void (*TickFct)());
-void schRemoveTask(u8 task_num);
->>>>>>> 91c65aa (First implementation of a non preemptive Scheduler)
-=======
-    uint32_t period;            // Rate at which the task should tick
-    uint32_t elapsedTime;       // Time since task's last tick
-    void (*TickFct)(void); // Function to call for task's tick
-} task;
-
-const int16_t schAddTask(const uint32_t period, void (*TickFct)());
-const int16_t schAddIndexTask(const uint32_t period, uint8_t task_num, void (*TickFct)());
-void schRemoveTask(uint8_t task_num);
->>>>>>> 2e4e481 (Remove unused header from scheduler)
 void schTickTask(void);
 void schRunTask(void);
 
--- src/scheduler.c
+++ src/scheduler.c
@@ -7,21 +7,11 @@
  */
 
 #include "scheduler.h"
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 #include <stddef.h>
->>>>>>> 91c65aa (First implementation of a non preemptive Scheduler)
-=======
->>>>>>> 2e4e481 (Remove unused header from scheduler)
 
 task tasks[SCH_NUM_TASK] = {0};
 
 /**
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 2e4e481 (Remove unused header from scheduler)
  * @addtogroup VGA-Interface
  * @{
  * @addtogroup Scheduler
@@ -29,36 +19,15 @@ task tasks[SCH_NUM_TASK] = {0};
  */
 
 /**
-<<<<<<< HEAD
-=======
->>>>>>> 91c65aa (First implementation of a non preemptive Scheduler)
-=======
->>>>>>> 2e4e481 (Remove unused header from scheduler)
  * @brief Add task to task array to be scheduled
  *
  * @param period system tick interval after witch the function should be called
  * @param TickFct pointer to the function to call
-<<<<<<< HEAD
-<<<<<<< HEAD
  * @return int16_t location in the array
  */
 const int16_t schAddTask(const uint32_t period, void (*TickFct)())
 {
     for (int8_t task_num = 0; task_num < SCH_NUM_TASK; task_num++)
-=======
- * @return u8 location in the array
-=======
- * @return int16_t location in the array
->>>>>>> 2e4e481 (Remove unused header from scheduler)
- */
-const int16_t schAddTask(const uint32_t period, void (*TickFct)())
-{
-<<<<<<< HEAD
-    for (u8 task_num = 0; task_num < SCH_NUM_TASK; task_num++)
->>>>>>> 91c65aa (First implementation of a non preemptive Scheduler)
-=======
-    for (int8_t task_num = 0; task_num < SCH_NUM_TASK; task_num++)
->>>>>>> 2e4e481 (Remove unused header from scheduler)
     {
         if (tasks[task_num].TickFct == NULL)
         {
@@ -77,21 +46,9 @@ const int16_t schAddTask(const uint32_t period, void (*TickFct)())
  * @param period system tick interval at witch the function should be called
  * @param task_num index of the task
  * @param TickFct pointer to the function to call
-<<<<<<< HEAD
-<<<<<<< HEAD
  * @return int16_t location in the array
  */
 const int16_t schAddIndexTask(const uint32_t period, uint8_t task_num, void (*TickFct)())
-=======
- * @return u8 location in the array
- */
-sc16 schAddIndexTask(uc32 period, u8 task_num, void (*TickFct)())
->>>>>>> 91c65aa (First implementation of a non preemptive Scheduler)
-=======
- * @return int16_t location in the array
- */
-const int16_t schAddIndexTask(const uint32_t period, uint8_t task_num, void (*TickFct)())
->>>>>>> 2e4e481 (Remove unused header from scheduler)
 {
     tasks[task_num].period = period;
     tasks[task_num].elapsedTime = 0;
@@ -104,15 +61,7 @@ const int16_t schAddIndexTask(const uint32_t period, uint8_t task_num, void (*Ti
  *
  * @param task_num task index in the array
  */
-<<<<<<< HEAD
-<<<<<<< HEAD
 void schRemoveTask(uint8_t task_num)
-=======
-void schRemoveTask(u8 task_num)
->>>>>>> 91c65aa (First implementation of a non preemptive Scheduler)
-=======
-void schRemoveTask(uint8_t task_num)
->>>>>>> 2e4e481 (Remove unused header from scheduler)
 {
     tasks[task_num].period = 0;
     tasks[task_num].elapsedTime = 0;
@@ -124,21 +73,9 @@ void schRemoveTask(uint8_t task_num)
  *
  * @param task_num task index in the array
  */
-<<<<<<< HEAD
-<<<<<<< HEAD
 void schRemoveAllTask(int8_t task_num)
 {
     for (int8_t task_num = 0; task_num < SCH_NUM_TASK; task_num++)
-=======
-void schRemoveAllTask(u8 task_num)
-{
-    for (u8 task_num = 0; task_num < SCH_NUM_TASK; task_num++)
->>>>>>> 91c65aa (First implementation of a non preemptive Scheduler)
-=======
-void schRemoveAllTask(int8_t task_num)
-{
-    for (int8_t task_num = 0; task_num < SCH_NUM_TASK; task_num++)
->>>>>>> 2e4e481 (Remove unused header from scheduler)
     {
 
         tasks[task_num].period = 0;
@@ -153,15 +90,7 @@ void schRemoveAllTask(int8_t task_num)
  */
 void schTickTask(void)
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
     for (int8_t task_num = 0; task_num < SCH_NUM_TASK; task_num++)
-=======
-    for (u8 task_num = 0; task_num < SCH_NUM_TASK; task_num++)
->>>>>>> 91c65aa (First implementation of a non preemptive Scheduler)
-=======
-    for (int8_t task_num = 0; task_num < SCH_NUM_TASK; task_num++)
->>>>>>> 2e4e481 (Remove unused header from scheduler)
     {
         if (tasks[task_num].TickFct != NULL)
         {
@@ -176,15 +105,7 @@ void schTickTask(void)
  */
 void schRunTask(void)
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
     for (int8_t task_num = 0; task_num < SCH_NUM_TASK; task_num++)
-=======
-    for (u8 task_num = 0; task_num < SCH_NUM_TASK; task_num++)
->>>>>>> 91c65aa (First implementation of a non preemptive Scheduler)
-=======
-    for (int8_t task_num = 0; task_num < SCH_NUM_TASK; task_num++)
->>>>>>> 2e4e481 (Remove unused header from scheduler)
     {
         if (tasks[task_num].TickFct != NULL &&
             tasks[task_num].period <= tasks[task_num].elapsedTime)
@@ -193,16 +114,6 @@ void schRunTask(void)
             tasks[task_num].TickFct();
         }
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
 }
 ///@}
 ///@}
-=======
-}
->>>>>>> 91c65aa (First implementation of a non preemptive Scheduler)
-=======
-}
-///@}
-///@}
->>>>>>> 2e4e481 (Remove unused header from scheduler)
